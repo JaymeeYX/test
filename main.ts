b7fbb9f5d16abee325812018eeb03f69ec2893d6
JaymeeYX <87964859+JaymeeYX@users.noreply.github.com>
2021-07-27 11:12:43 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+basic.forever(function () {
+    music.playMelody("C G C G G A G F ", 120)
+    music.playMelody("E D C C C G C G ", 120)
+    music.playMelody("G A G F A G - C ", 120)
+    music.playMelody("G C G - G F E C ", 120)
+})
+basic.forever(function () {
+    basic.showString("/////////////////////")
+    basic.showString("////////////////////////////")
+    basic.showString("///////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\||||||")
+    basic.showString("///\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\||||||||||||\\\\\\\\\\\\\\///////")
+})

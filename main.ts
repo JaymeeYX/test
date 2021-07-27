010dcd0d46f08667b1ec6939ee6898a6d7173453
JaymeeYX <87964859+JaymeeYX@users.noreply.github.com>
2021-07-27 11:20:10 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,9 @@
 basic.forever(function () {
     music.playMelody("C G C G G A G F ", 120)
-    music.playMelody("E D C C C G C G ", 120)
-    music.playMelody("G A G F A G - C ", 120)
-    music.playMelody("G C G - G F E C ", 120)
+    music.playMelody("E D C - C G C G ", 120)
+    music.playMelody("G A G F A F - C ", 120)
+    music.playMelody("G C G - G A G F ", 120)
+    music.playMelody("C E D C G C G C ", 120)
 })
 basic.forever(function () {
     basic.showString("/////////////////////")
